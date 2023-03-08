fa60e75998f7c28055b4e6b4d966f916a1421ee7
checkforants <72790509+checkforants@users.noreply.github.com>
2023-03-09 00:45:54 +0300

    draft of draggable column

--- src/components/Elements/Elements.tsx
+++ src/components/Elements/Elements.tsx
@@ -1,14 +1,58 @@
 import { useAppSelector } from "../../hooks";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { useState } from "react";
 
-// @flow 
-type Props = {
-	
-};
+// @flow
+type Props = {};
 export const Elements = (props: Props) => {
-	const counter = useAppSelector(store=> store?.value);
-	return (
-		<div>
-			asdasdasd
-		</div>
-	);
+  const counter = useAppSelector((store) => store?.value);
+  const [lst, setLst] = useState<any>([{ id: 1 }, { id: 2 }, { id: 3 }]);
+  const reorder = (list: any, startIndex: any, endIndex: any) => {
+    console.log("yeeap");
+
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+  const onDragEnd = (result: any) => {
+    console.log(123123123);
+
+    if (!result.destination) {
+      return;
+    }
+    const items = reorder(lst, result.source.index, result.destination.index);
+    setLst(items);
+  };
+  // ref={provided.innerRef}{...provided.dragHandleProps}{...provided.draggableProps}
+  return (
+    <div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="0">
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {lst.map((el: any, step: any) => {
+                return (
+                  <Draggable draggableId={`${el.id}`} index={step} key={el.id}>
+                    {(provided) => (
+                      <div
+                        className="p-10 border-2 border-red-500 mb-2"
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                      >
+                        {el.id}
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,20 +1,19 @@
-import { createStore } from 'redux'
-import {PayloadAction} from '@reduxjs/toolkit'
+import { createStore } from "redux";
+import { PayloadAction } from "@reduxjs/toolkit";
 
 function counterReducer(state = { value: 0 }, action: PayloadAction) {
   switch (action.type) {
-    case 'counter/incremented':
-      return { value: state.value + 1 }
-    case 'counter/decremented':
-      return { value: state.value - 1 }
+    case "counter/incremented":
+      return { value: state.value + 1 };
+    case "counter/decremented":
+      return { value: state.value - 1 };
     default:
-      return state
+      return state;
   }
 }
 
 export const store = createStore(counterReducer);
-store.subscribe(() => console.log(store.getState()))
+store.subscribe(() => console.log(store.getState()));
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
